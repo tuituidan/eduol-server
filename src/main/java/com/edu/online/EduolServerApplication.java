1fda70eb06de12e7bb75cc5ba77354a7be21c505
zhujunhan <136375002@qq.com>
2020-06-30 22:17:07 +0800

    初始

---
+++ src/main/java/com/edu/online/EduolServerApplication.java
@@ -0,0 +1,13 @@
+package com.edu.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduolServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduolServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edu/online/EduolServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduolServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
